846b615913315c0ecaf83daf398f79536e229e57
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2023-12-29 13:50:16 +0530

    Add files via upload

---
+++ ha-flutter-app/ios/Runner/AppDelegate.swift
@@ -0,0 +1,32 @@
+import UIKit
+import Flutter
+import Firebase
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure() //add this before the code below
+    self.window.makeSecure()
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+//And this extension
+extension UIWindow {
+func makeSecure() {
+    let field = UITextField()
+    field.isSecureTextEntry = true
+    self.addSubview(field)
+    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    self.layer.superlayer?.addSublayer(field.layer)
+    field.layer.sublayers?.first?.addSublayer(self.layer)
+  }
+}
